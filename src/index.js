2987709ee7f515dcc145b026e4302072fad8a7fb
Max Xu <max.xu@biqmind.com>
2019-05-16 00:57:41 +0800

    add reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
 
-serviceWorker.register();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ src/reducers/users.js
@@ -0,0 +1,13 @@
+import { GET_USERS } from '../actions/users'
+
+export default function questions (state = {}, action) {
+  switch(action.type) {
+    case GET_USERS :
+      return {
+        ...state,
+        ...action.users,
+      }
+    default :
+      return state
+  }
+}
