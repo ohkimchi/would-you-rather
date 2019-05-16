91362a90fc407051ca17a2afa79e3bc0493ad6f4
Max Xu <max.xu@biqmind.com>
2019-05-16 14:05:51 +0800

    remove the loading bar element and solve the error next: construct the homepage with all the questions listed out then: create the form that will enable user to ask new qs

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Leaderboard from './Leaderboard'
-import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
+
   render() {
     return (
       <div className="App">
-      <LoadingBar />
         {this.props.loading === true ? null : <Leaderboard /> }
       </div>
     )
   }
 }
 
-function mapStateToProps({ authedUser }) {
-  return {
-    loading: authedUser === null
-  }
-}
-
-export default connect(mapStateToProps)(App);
+export default connect()(App);
