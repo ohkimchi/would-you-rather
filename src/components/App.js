136c4b7eecf5413bf23d46e690711155cf306cb8
Max Xu <max.xu@biqmind.com>
2019-05-16 18:15:04 +0800

    minor changes for testing

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 // import Leaderboard from './Leaderboard'
-import Homepage from './Homepage'
+// import Homepage from './Homepage'
 import LoadingBar from 'react-redux-loading'
 import NewQuestion from './NewQuestion'
 
