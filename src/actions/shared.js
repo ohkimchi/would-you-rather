f54cf5bdf5aa6225f6965b83955822d782fedad8
Max Xu <max.xu@biqmind.com>
2019-05-16 14:10:02 +0800

    remove the loading bar related lines

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,19 +2,19 @@ import { getInitialData } from '../utils/api'
 import { getUsers } from '../actions/users'
 import { getQuestions } from '../actions/questions'
 import { setAuthedUser } from '../actions/authedUser'
-import { showLoading, hideLoading } from 'react-redux-loading'
+// import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHED_ID = 'sarahedo'
 
 export function handleInitialData () {
   return (dispatch) => {
-    dispatch(showLoading())
+    // dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(getUsers(users))
         dispatch(getQuestions(questions))
         dispatch(setAuthedUser(AUTHED_ID))
-        dispatch(hideLoading())
+        // dispatch(hideLoading())
       })
   }
 }
